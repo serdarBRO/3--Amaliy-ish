8314764734f703f6880a730141eac85ac24d82d9
serdarBRO <116366557+serdarBRO@users.noreply.github.com>
2022-11-02 14:15:15 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Кubning qirrasining uzunligi berilgan.
+// Кubning hajmini va yon sirtining yuzini 
+// hisoblash algoritmini tuzing.
+
+let R=prompt(`R ni kiritng:`);
+let L=2*3.14*R;
+console.log(`UZUNLIK=${L}`);
